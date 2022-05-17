df9c5266f0e8ff7f5e115a9a639154203b0687bb
Vip4h <vibore.anacletus@gmail.com>
2022-05-17 10:57:25 +0100

    first commit

---
+++ AprendoConJuli.js
@@ -0,0 +1,9 @@
+/*
+El número tiene que estar entre 0 y 99999999
+Debes calcular el resto de la division entre el nº y el 23 --> el resto tendrá que estar entre 0-22.. si quieres que te de el resto, se hace con "%"
+Dependiendo de cual te de, T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E (meter en un array, sin comas)
+Si la letra del DNI es correcta, te indica que lo es y si no, lo contrario*/
+
+let letra = ("T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E");
+let Dni = "65873422G";
+let dniNumero = "65873422"
